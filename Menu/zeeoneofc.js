d15f316e337c6d178ff3d94ed2ae67d1a7ee33a7
Nozzyソ <94677706+NozyxN@users.noreply.github.com>
2021-11-21 00:33:35 +0700

    Tq :3

--- Menu/zeeoneofc.js
+++ Menu/zeeoneofc.js
@@ -1,177 +1,177 @@
 exports.allmenu = (pushname, sender, isOwner, isPrem, cekvipp, getLimit, limitawal, limit, balance, getBalance, simbolnya, petik, prefix) => {
 return`Hello ${pushname}🤗
 
-📛 Nama : ${pushname} 
-🆔 Nomor : @${sender.split("@")[0]} 
-📊 Status : ${isOwner?`Owner ⚔️`:isPrem?`Premium 🏅`:`Free User`} 
-🎁 Expired Prem : ${isOwner?`Unlimited Owner`:isPrem ? `${cekvipp.days}d, ${cekvipp.hours}h, ${cekvipp.minutes}m, ${cekvipp.seconds}s`:'Not Premium'}
-💳 Limit : ${isOwner?`Unlimited`: isPrem ? `Unlimited Premium` : getLimit(sender, limitawal, limit) +' / '+limitawal}
-💹 Balance : $${getBalance(sender, balance)}
+• Nama : ${pushname} 
+• Nomor : @${sender.split("@")[0]} 
+• Status : ${isOwner?`Owner ⚔️`:isPrem?`Premium 🏅`:`Free User`} 
+• Expired Prem : ${isOwner?`Unlimited Owner`:isPrem ? `${cekvipp.days}d, ${cekvipp.hours}h, ${cekvipp.minutes}m, ${cekvipp.seconds}s`:'Not Premium'}
+• Limit : ${isOwner?`Unlimited`: isPrem ? `Unlimited Premium` : getLimit(sender, limitawal, limit) +' / '+limitawal}
+• Balance : $${getBalance(sender, balance)}
 
 
 *Owner Menu*${petik}
-${simbolnya} ${prefix}bc
-${simbolnya} ${prefix}exif
-${simbolnya} ${prefix}ban
-${simbolnya} ${prefix}unban
-${simbolnya} ${prefix}block
-${simbolnya} ${prefix}unblock
-${simbolnya} ${prefix}clearall
-${simbolnya} ${prefix}setbiobot
-${simbolnya} ${prefix}setnamabot
-${simbolnya} ${prefix}resetlimit
-${simbolnya} ${prefix}cekapikey
-${simbolnya} ${prefix}setthumb
-${simbolnya} ${prefix}setmenu [simple/ori]
-${simbolnya} ${prefix}prem add @tag waktu
-${simbolnya} ${prefix}prem del @tag waktu
-${simbolnya} ${prefix}sewa add waktu
-${simbolnya} ${prefix}sewa del
-${simbolnya} ${prefix}auto ngetik/vn
-${simbolnya} ${prefix}setprefix nopref/multi
-${simbolnya}  > || x
-${simbolnya}  =>
-${simbolnya}  $
-${simbolnya}  <${petik}
+• ${prefix}bc
+• ${prefix}exif
+• ${prefix}ban
+• ${prefix}unban
+• ${prefix}block
+• ${prefix}unblock
+• ${prefix}clearall
+• ${prefix}setbiobot
+• ${prefix}setnamabot
+• ${prefix}resetlimit
+• ${prefix}cekapikey
+• ${prefix}setthumb
+• ${prefix}setmenu [simple/ori]
+• ${prefix}prem add @tag waktu
+• ${prefix}prem del @tag waktu
+• ${prefix}sewa add waktu
+• ${prefix}sewa del
+• ${prefix}auto ngetik/vn
+• ${prefix}setprefix nopref/multi
+•  > || x
+•  =>
+•  $
+•  <${petik}
 
 *Download Menu*${petik}
-${simbolnya} ${prefix}play [query]
-${simbolnya} ${prefix}ytmp3 [url]
-${simbolnya} ${prefix}ytmp4 [url]
-${simbolnya} ${prefix}video [query]
-${simbolnya} ${prefix}igdl [url]
-${simbolnya} ${prefix}igstory [username]
-${simbolnya} ${prefix}igphoto [url]
-${simbolnya} ${prefix}igreel [url]
-${simbolnya} ${prefix}igvideo [url]
-${simbolnya} ${prefix}igtv [url]
-${simbolnya} ${prefix}tiktok [url]
-${simbolnya} ${prefix}tiktokaudio [url]
-${simbolnya} ${prefix}tiktoknowm [url]
-${simbolnya} ${prefix}mediafire [url]${petik}
+• ${prefix}play [query]
+• ${prefix}ytmp3 [url]
+• ${prefix}ytmp4 [url]
+• ${prefix}video [query]
+• ${prefix}igdl [url]
+• ${prefix}igstory [username]
+• ${prefix}igphoto [url]
+• ${prefix}igreel [url]
+• ${prefix}igvideo [url]
+• ${prefix}igtv [url]
+• ${prefix}tiktok [url]
+• ${prefix}tiktokaudio [url]
+• ${prefix}tiktoknowm [url]
+• ${prefix}mediafire [url]${petik}
 
 *Convert Menu*${petik}
-${simbolnya} ${prefix}sticker
-${simbolnya} ${prefix}swm
-${simbolnya} ${prefix}take
-${simbolnya} ${prefix}attp
-${simbolnya} ${prefix}semoji 🐦
-${simbolnya} ${prefix}toimg
-${simbolnya} ${prefix}tomp4
-${simbolnya} ${prefix}togif
-${simbolnya} ${prefix}tomp3
-${simbolnya} ${prefix}tovn
-${simbolnya} ${prefix}tinyurl
-${simbolnya} ${prefix}kontak
-${simbolnya} ${prefix}tts Kode Bahasa Teks
-${simbolnya} ${prefix}translate Kode Bahasa
-${simbolnya} ${prefix}bass
-${simbolnya} ${prefix}slow
-${simbolnya} ${prefix}fast
-${simbolnya} ${prefix}robot
-${simbolnya} ${prefix}detikvn
-${simbolnya} ${prefix}tupai
-${simbolnya} ${prefix}vibrato${petik}
+• ${prefix}sticker
+• ${prefix}swm
+• ${prefix}take
+• ${prefix}attp
+• ${prefix}semoji 🐦
+• ${prefix}toimg
+• ${prefix}tomp4
+• ${prefix}togif
+• ${prefix}tomp3
+• ${prefix}tovn
+• ${prefix}tinyurl
+• ${prefix}kontak
+• ${prefix}tts Kode Bahasa Teks
+• ${prefix}translate Kode Bahasa
+• ${prefix}bass
+• ${prefix}slow
+• ${prefix}fast
+• ${prefix}robot
+• ${prefix}detikvn
+• ${prefix}tupai
+• ${prefix}vibrato${petik}
 
 *Editor Menu*${petik}
-${simbolnya} ${prefix}memegen teks atas | teks bawah
-${simbolnya} ${prefix}stickmeme teks${petik}
+• ${prefix}memegen teks atas | teks bawah
+• ${prefix}stickmeme teks${petik}
 
 *Search Menu*${petik}
-${simbolnya} ${prefix}caripesan
-${simbolnya} ${prefix}pinterest
-${simbolnya} ${prefix}wikipedia
-${simbolnya} ${prefix}playstore
-${simbolnya} ${prefix}brainly
-${simbolnya} ${prefix}yts${petik}
+• ${prefix}caripesan
+• ${prefix}pinterest
+• ${prefix}wikipedia
+• ${prefix}playstore
+• ${prefix}brainly
+• ${prefix}yts${petik}
 
 *Storage Menu*${petik}
-${simbolnya} ${prefix}addrespon
-${simbolnya} ${prefix}delrespon
-${simbolnya} ${prefix}listrespon
-${simbolnya} ${prefix}listbadword
-${simbolnya} ${prefix}addbadword
-${simbolnya} ${prefix}delbadword
-${simbolnya} ${prefix}addvn
-${simbolnya} ${prefix}listvn
-${simbolnya} ${prefix}delvn
-${simbolnya} ${prefix}addimage
-${simbolnya} ${prefix}listimage
-${simbolnya} ${prefix}delimage
-${simbolnya} ${prefix}addvideo
-${simbolnya} ${prefix}listvideo
-${simbolnya} ${prefix}delvideo
-${simbolnya} ${prefix}addsticker
-${simbolnya} ${prefix}liststicker
-${simbolnya} ${prefix}delstik${petik}
+• ${prefix}addrespon
+• ${prefix}delrespon
+• ${prefix}listrespon
+• ${prefix}listbadword
+• ${prefix}addbadword
+• ${prefix}delbadword
+• ${prefix}addvn
+• ${prefix}listvn
+• ${prefix}delvn
+• ${prefix}addimage
+• ${prefix}listimage
+• ${prefix}delimage
+• ${prefix}addvideo
+• ${prefix}listvideo
+• ${prefix}delvideo
+• ${prefix}addsticker
+• ${prefix}liststicker
+• ${prefix}delstik${petik}
 
 *Fun Menu*${petik}
-${simbolnya} ${prefix}slot
-${simbolnya} ${prefix}truth
-${simbolnya} ${prefix}dare
-${simbolnya} ${prefix}voting @tag | alasan | waktu
-${simbolnya} ${prefix}delvote
+• ${prefix}slot
+• ${prefix}truth
+• ${prefix}dare
+• ${prefix}voting @tag | alasan | waktu
+• ${prefix}delvote
 	vote
 	devote${petik}
    
 *Group Menu*${petik}
-${simbolnya} ${prefix}chatinfo
-${simbolnya} ${prefix}afk reason
-${simbolnya} ${prefix}hidetag
-${simbolnya} ${prefix}notif
-${simbolnya} ${prefix}infogc
-${simbolnya} ${prefix}jadian
-${simbolnya} ${prefix}listonline
-${simbolnya} ${prefix}tagall
-${simbolnya} ${prefix}ceksewa
-${simbolnya} ${prefix}add 628XX
-${simbolnya} ${prefix}kick
-${simbolnya} ${prefix}promote
-${simbolnya} ${prefix}demote
-${simbolnya} ${prefix}resetlink
-${simbolnya} ${prefix}linkgc
-${simbolnya} ${prefix}grup [open/close]
-${simbolnya} ${prefix}antibadword [on/off]
-${simbolnya} ${prefix}antilink [on/off]
-${simbolnya} ${prefix}antitroli [on/off]${petik}
+• ${prefix}chatinfo
+• ${prefix}afk reason
+• ${prefix}hidetag
+• ${prefix}notif
+• ${prefix}infogc
+• ${prefix}jadian
+• ${prefix}listonline
+• ${prefix}tagall
+• ${prefix}ceksewa
+• ${prefix}add 628XX
+• ${prefix}kick
+• ${prefix}promote
+• ${prefix}demote
+• ${prefix}resetlink
+• ${prefix}linkgc
+• ${prefix}grup [open/close]
+• ${prefix}antibadword [on/off]
+• ${prefix}antilink [on/off]
+• ${prefix}antitroli [on/off]${petik}
    
 *Limit Menu*${petik}
-${simbolnya} ${prefix}topbalance
-${simbolnya} ${prefix}toplimit
-${simbolnya} ${prefix}limit
-${simbolnya} ${prefix}level
-${simbolnya} ${prefix}balance
-${simbolnya} ${prefix}buylimit Nominal
-${simbolnya} ${prefix}giftlimit @tag | nominal
-${simbolnya} ${prefix}buyprem [need 1k limit]${petik}
+• ${prefix}topbalance
+• ${prefix}toplimit
+• ${prefix}limit
+• ${prefix}level
+• ${prefix}balance
+• ${prefix}buylimit Nominal
+• ${prefix}giftlimit @tag | nominal
+• ${prefix}buyprem [need 1k limit]${petik}
    
 *Maker Menu*${petik}
-${simbolnya} ${prefix}nuliskanan
-${simbolnya} ${prefix}nuliskiri
-${simbolnya} ${prefix}foliokanan
-${simbolnya} ${prefix}foliokiri
-${simbolnya} ${prefix}naruto
-${simbolnya} ${prefix}shadow
-${simbolnya} ${prefix}lovemsg
-${simbolnya} ${prefix}doubleheart
-${simbolnya} ${prefix}coffecup
-${simbolnya} ${prefix}lovetext
-${simbolnya} ${prefix}butterfly
-${simbolnya} ${prefix}romantic
-${simbolnya} ${prefix}smoke
-${simbolnya} ${prefix}burnpaper
-${simbolnya} ${prefix}grass${petik}
+• ${prefix}nuliskanan
+• ${prefix}nuliskiri
+• ${prefix}foliokanan
+• ${prefix}foliokiri
+• ${prefix}naruto
+• ${prefix}shadow
+• ${prefix}lovemsg
+• ${prefix}doubleheart
+• ${prefix}coffecup
+• ${prefix}lovetext
+• ${prefix}butterfly
+• ${prefix}romantic
+• ${prefix}smoke
+• ${prefix}burnpaper
+• ${prefix}grass${petik}
 
 *Others Menu*${petik}
-${simbolnya} ${prefix}pantun
-${simbolnya} ${prefix}faktaunik
-${simbolnya} ${prefix}styletext
-${simbolnya} ${prefix}donasi
-${simbolnya} ${prefix}asupan
-${simbolnya} ${prefix}nulis
-${simbolnya} ${prefix}getnumber
-${simbolnya} ${prefix}getpic
-${simbolnya} ${prefix}inspect
-${simbolnya} ${prefix}wame 
-${simbolnya} ${prefix}towame${petik}`
+• ${prefix}pantun
+• ${prefix}faktaunik
+• ${prefix}styletext
+• ${prefix}donasi
+• ${prefix}asupan
+• ${prefix}nulis
+• ${prefix}getnumber
+• ${prefix}getpic
+• ${prefix}inspect
+• ${prefix}wame 
+• ${prefix}towame${petik}`
 }
